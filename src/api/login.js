f2d2635eeee6073a445a8380eb74797947d70177
comlejade <thundersmile@126.com>
2021-03-24 19:52:56 +0800

    add comment

--- src/api/login.js
+++ src/api/login.js
@@ -1,16 +1,32 @@
 import axios from '@/util/request'
 
+/**
+ * 获取验证码接口
+ * @param {*} sid 唯一标识
+ * @returns Promise
+ */
 const getCode = (sid) => {
   return axios.get('/public/getCaptcha', {
     params: { sid }
   })
 }
 
+/**
+ * 找回密码接口
+ * @param {*} options 用户信息
+ * @returns Promise
+ */
 const forget = (options) => {
   return axios.post('/login/forget', {
     ...options
   })
 }
+
+/**
+ * 登录接口
+ * @param {*} options 用户信息
+ * @returns Promise
+ */
 
 const login = (options) => {
   return axios.post('/login/login', {
